e2c18bfa579ce225e5df8c6d4c3c77d58bba0a85
Dev.MJ <mr.lucifers@gmail.com>
2017-03-29 20:10:01 +0900

    Initial commit :)

---
+++ ManualJayout/ManualJayout.h
@@ -0,0 +1,19 @@
+//
+//  ManualJayout.h
+//  ManualJayout
+//
+//  Created by Dev.MJ on 29/03/2017.
+//  Copyright © 2017 MJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ManualJayout.
+FOUNDATION_EXPORT double ManualJayoutVersionNumber;
+
+//! Project version string for ManualJayout.
+FOUNDATION_EXPORT const unsigned char ManualJayoutVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ManualJayout/PublicHeader.h>
+
+
